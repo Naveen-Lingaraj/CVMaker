ad61cb7e80291737b7a30902e28f0cc944f78954
Naveen Lingaraj <Naveen.Lingaraj@ey.com>
2020-01-03 21:41:42 +0530

    Core and Infrastructure solution folders

---
+++ CVMaker/CVMaker.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CVMaker.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ CVMaker/CVMaker.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CVMaker.Domain
+{
+    public class Class1
+    {
+    }
+}
